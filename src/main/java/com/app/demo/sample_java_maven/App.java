f7de4912e8a9af70ab8b55e088501d899775b45f
Saloni Sharma <salonisharma9700@gmail.com>
2024-11-16 13:20:40 +0530

    MavenProject

---
+++ src/main/java/com/app/demo/sample_java_maven/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.sample_java_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
